83724b60551c46de9fd393b61b685db98af248cc
akshay-ken <akshaybest3120@gmail.com>
2024-09-26 23:39:27 +0530

    s10c19

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -1,20 +1,27 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace Framework;
 
-class App {
+class App
+{
     private Router $router;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->router = new Router();
     }
 
-    public function run() {
-        echo "App is running. so catch it.";
+    public function run()
+    {
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method);
     }
-    public function get(string $path, array $controller) {
+    public function get(string $path, array $controller)
+    {
         $this->router->add('GET', $path, $controller);
     }
 }
