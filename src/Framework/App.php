bb3661da620fc46e5a977784d00fdd2c2eb357bf
akshay-ken <akshaybest3120@gmail.com>
2024-09-28 19:39:08 +0530

    S12L5

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -7,10 +7,17 @@ namespace Framework;
 class App
 {
     private Router $router;
+    private Container $container; # to create instance of Container class from App class
 
-    public function __construct()
+    public function __construct(string $containerDefinitionsPath = null)
     {
         $this->router = new Router();
+        $this->container = new Container();
+
+        if ($containerDefinitionsPath) {
+            $containerDefinitions = include $containerDefinitionsPath;
+            $this->container->addDefinitions($containerDefinitions);
+        }
     }
 
     public function run()
