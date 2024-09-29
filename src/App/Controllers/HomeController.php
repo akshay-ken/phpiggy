91295d404f63890d777d18ee74e316895233b457
akshay-ken <akshaybest3120@gmail.com>
2024-09-29 21:14:46 +0530

    S12L12

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -10,12 +10,7 @@ use App\Config\Paths;
 
 class HomeController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
-    {
-        $this->view = new TemplateEngine(Paths::VIEW);
-    }
+    public function __construct(private TemplateEngine $view) {}
     public function home()
     {
 
