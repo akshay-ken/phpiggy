1c0adf78ca57a538c31cda1319bfa04b54b8099b
akshay-ken <akshaybest3120@gmail.com>
2024-10-08 10:33:47 +0530

    s13l20

---
+++ src/App/Middleware/SessionMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SessionException;
+
+class SessionMiddleware implements MiddlewareInterface
+{
+    public function process(callable $next)
+    {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("Session Already Active.");
+        }
+        if (headers_sent($filename, $line)) {
+            throw new SessionException("Headers Already Sent. Filename {$filename} - Line{$line}");
+        }
+
+        session_start();
+
+        $next();
+
+        session_write_close();
+    }
+}
