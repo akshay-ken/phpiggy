66e5d47cd00670f7a67e1e936e4c67cbcf06617e
akshay-ken <akshaybest3120@gmail.com>
2024-10-15 09:21:11 +0530

    s14l15

--- cli.php
+++ cli.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Working script";
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database('mysql', [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy'
+], 'root', '');
+
+echo "Connected to database";
