0edb2bc46524dbdbb2438f9eb0c2024fcf5c7110
akshay-ken <akshaybest3120@gmail.com>
2024-10-22 22:27:30 +0530

    s14l23

--- cli.php
+++ cli.php
@@ -10,4 +10,27 @@ $db = new Database('mysql', [
     'dbname' => 'phpiggy'
 ], 'root', '');
 
-echo "Connected to database";
+try {
+
+    $db->connection->beginTransaction();
+    $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
+
+    $search = "Hats";
+    $query = "SELECT * FROM products WHERE name=:name";
+
+    $stmt = $db->connection->prepare($query);
+
+    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+
+    $db->connection->commit();
+} catch (Exception $error) {
+    if ($db->connection->inTransaction()) {
+        $db->connection->rollBack();
+    }
+
+    echo "Transaction Failed.";
+}
