3c444445465e4750555b4f99bbed5251ae0566f2
akshay-ken <akshaybest3120@gmail.com>
2024-10-28 23:22:46 +0530

    s16l7

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
-use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule, MatchRule};
+use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule, MatchRule, LengthMaxRule, NumericRule};
 
 class ValidatorService
 {
@@ -20,6 +20,8 @@ class ValidatorService
         $this->validator->add('in', new InRule());
         $this->validator->add('url', new UrlRule());
         $this->validator->add('match', new MatchRule());
+        $this->validator->add('lengthMax', new LengthMaxRule());
+        $this->validator->add('numeric', new NumericRule());
     }
     public function ValidateRegister(array $formData)
     {
@@ -38,6 +40,14 @@ class ValidatorService
         $this->validator->validate($formData, [
             'email' => ['required', 'email'],
             'password' => ['required']
+        ]);
+    }
+    public function validateTransaction(array $formData)
+    {
+        $this->validator->validate($formData, [
+            'description' => ['required', 'lengthMax:255'],
+            'amount' => ['required', 'numeric'],
+            'date' => ['required']
         ]);
     }
 }
