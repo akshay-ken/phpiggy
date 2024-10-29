e198f499a4e3667a9c846f9e97c6d0ab0ff7bcd8
akshay-ken <akshaybest3120@gmail.com>
2024-10-29 18:09:17 +0530

    s16l9

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
-use Framework\Rules\{RequiredRule, EmailRule, MinRule, InRule, UrlRule, MatchRule, LengthMaxRule, NumericRule};
+use Framework\Rules\{
+    RequiredRule,
+    EmailRule,
+    MinRule,
+    InRule,
+    UrlRule,
+    MatchRule,
+    LengthMaxRule,
+    NumericRule,
+    DateFormatRule
+};
 
 class ValidatorService
 {
@@ -22,6 +32,7 @@ class ValidatorService
         $this->validator->add('match', new MatchRule());
         $this->validator->add('lengthMax', new LengthMaxRule());
         $this->validator->add('numeric', new NumericRule());
+        $this->validator->add('dateFormat', new DateFormatRule());
     }
     public function ValidateRegister(array $formData)
     {
@@ -47,7 +58,7 @@ class ValidatorService
         $this->validator->validate($formData, [
             'description' => ['required', 'lengthMax:255'],
             'amount' => ['required', 'numeric'],
-            'date' => ['required']
+            'date' => ['required', 'dateFormat:Y-m-d']
         ]);
     }
 }
